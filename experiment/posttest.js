652b6b5bccc9f7f36e653c90558cb86140acb1c8
Parassingh-90 <69252670+Parassingh-90@users.noreply.github.com>
2020-08-23 12:54:20 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -90,23 +90,23 @@
 
   const myQuestions = [
     {
-      question: "This is question 1?",  ///// Write the question inside double quotes
+      question: "Dexter wants to set up his own public and private keys. He chooses p = 23 and q = 19 with e = 283. Find d?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "11",                  ///// Write the option 1 inside double quotes
+        b: "7",                  ///// Write the option 2 inside double quotes
+        c: "37",                  ///// Write the option 3 inside double quotes
+        d: "19"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
     },
 
     {
-      question: "This is question 2?",  ///// Write the question inside double quotes
+      question: "For p = 11 and q = 19 and choose e=17. Apply RSA algorithm where message=5 and find the cipher text?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "92",                  ///// Write the option 1 inside double quotes
+        b: "80",                  ///// Write the option 2 inside double quotes
+        c: "67",                  ///// Write the option 3 inside double quotes
+        d: "39"                   ///// Write the option 4 inside double quotes
       },
       correctAnswer: "b"                ///// Write the correct option inside double quotes
     },                                  ///// To add more questions, copy the section below 
